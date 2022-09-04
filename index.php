f0f76fd028f3b50630903a4b001a8c1a5a1e0851
Carlos Federico <carlos.federico005@gmail.com>
2022-09-03 21:13:09 -0400

    Added growing capabilities to content.

--- index.php
+++ index.php
@@ -1,15 +1,70 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/generic.css">
+    <link rel="stylesheet" href="styles/header.css">
+    <link rel="stylesheet" href="styles/content.css">
+    <link rel="stylesheet" href="styles/footer.css">
     <title>PNVEV</title>
 </head>
-<body>
-<?php
-    echo "Hello World (" . phpversion() . ")";
-?>
-</body>
-</html>
 
+<body>
+    <div class="header">
+        <div class="header-content-centering header-logos">
+            <img src="images/logos-cabecera.svg" alt="logos">
+        </div>
+        <div class="header-content-centering header-title">
+            <h1>Tablero de Programa Nacional de Enfermedades Vectoriales</h1>
+        </div>
+        <div class="header-content-centering header-navbar">
+            <div class="navbar-item">
+                <a href="#">Tablero Principal</a>
+            </div>
+            <div class="navbar-item">
+                <a href="#">Fiebre Amarilla</a>
+            </div>
+            <div class="navbar-item">
+                <a href="#">Hantavirus</a>
+            </div>
+            <div class="navbar-item">
+                <a href="#">Malaria</a>
+            </div>
+            <div class="navbar-item">
+                <a href="#">Leptopirosis</a>
+            </div>
+            <div class="navbar-item">
+                <a href="#">Leishmaniasis</a>
+            </div>
+            <div class="navbar-item">
+                <a href="#">Chagas</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="content">
+    </div>
+
+    <div class="footer">
+        <div class="footer-item">
+            <h3>MÁS INFORMACIÓN</h3>
+        </div>
+        <div class="footer-item">
+            <h3>INTERÉS</h3>
+            <div class="footer-intereses">
+                <a href="#">
+                    <img src="images/logo-dgvs-white.svg" alt="">
+                </a>
+                <a href="#">
+                    <img src="images/logo-mspbs-white.svg" alt="">
+                </a>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
